d2b194a8af2d6c3000215b2a051e3dc388ed1f33
Brian Lauber <constructible.truth@gmail.com>
2014-11-05 20:53:43 -0500

    Added validations to Book

--- models/book.rb
+++ models/book.rb
@@ -2,4 +2,10 @@
 require 'active_record'
 
 class Book < ActiveRecord::Base
+  validates :title,
+            presence: true
+
+  validates :publication_date,
+            numericality: {only_integer: true}
+
 end
